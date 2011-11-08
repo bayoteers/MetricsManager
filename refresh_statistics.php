c12978cb856417f43b8d3889bd4c0fc3ca7d9da0
Kamil Marek <kamil.marek@gmail.com>
2011-11-08 15:56:58 +0200

    Adding 'directory tree' feature to refresh statistics tab
    Refresh_statistics.php
    ----------------------
    Adding directory tree with multiselection enabled.
    
    Known issues:
    Styling is lost after collapsing directory
    
    lib/user_settings.php
    ----------------------
    reverting last changes (were committed by accident)

--- refresh_statistics.php
+++ refresh_statistics.php
@@ -18,16 +18,63 @@
 # 1. Remove present-day raw_data files for selected statistics (one-by-one)
 # 2. Fetch data for selected statistics (one-by-one)
 */
+?>
+<script type="text/javascript">
+	var statistics_to_refresh = new Array();
+
+	$(document).ready( function() {				
+		$('#fileTree').fileTree({ 	
+			root: '<?php echo $path_to_statistics_directory; ?>', 
+			script: 'jqueryFileTree.php' }, 
+			function(file) { 
+				addSelectedToArray(file);
+			});
+		});
+
+function addSelectedToArray(file) {	
+	var idx = statistics_to_refresh.indexOf(file);
+	
+	if(idx != -1) {
+		document.getElementById(file).style.background = 'none';
+		statistics_to_refresh.splice(idx, 1);
+	}
+	else {
+		document.getElementById(file).style.background = '#68b3e3';
+		statistics_to_refresh.push(file);
+	}
+	
+	document.refresh_statistics.statistics_to_refresh.value = statistics_to_refresh;
+}
+</script>
+<?php
+
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['refresh_selected_statistics'])) {
 	$login = $_SESSION['logged in'];
 	system("sudo lib/libcontentaction.pl --start $login Refresh_statistics");
-	$list_of_statistics_to_refresh = $_POST['existing_list'];
+	$list_of_statistics_to_refresh = $_POST['statistics_to_refresh'];
+	$list_of_statistics_to_refresh = explode(",", $list_of_statistics_to_refresh);
 	$number_of_selected_statistics_to_refresh = count($list_of_statistics_to_refresh);
 	
 	if ($number_of_selected_statistics_to_refresh == 0) {
 		$log_info = 'You apparently didn\'t choose any statistics to refresh.';
 		logToSyslog($log_info);
 		displayMessageFieldset('ERROR!', $log_info, '?tab=refresh_statistics');
+	}
+	elseif ($number_of_selected_statistics_to_refresh == 1) {
+		system("sudo lib/libcontentaction.pl --refresh $list_of_statistics_to_refresh");
+		exec("sudo lib/libcontentaction.pl --fetch $statistics_user $fetch_statistics_from_bugzilla_file $list_of_statistics_to_refresh", &$output, &$return_var);
+		if ($return_var == 9) {
+			$body .= '<p>Updating' . $list_of_statistics_to_refresh;
+			$body .= '<br>Success! Selected statistics has been updated<br>';
+			$path = searchString($common_parameters_file, "STATS_URL_BASE") . '?s=' . searchString($list_of_statistics_to_refresh, "STATISTICS") . '&p=all';
+			$body .= '<a href = "' . $path . '" target="_blank">' . searchString($list_of_statistics_to_refresh, "STATISTICS") . '</a></p>';
+		}
+		
+		else {
+			$success = false;
+			$body .= 'Updating' . $list_of_statistics_to_refresh;
+			$body .= '<br>ERROR occured while updating statistics.<br><br>';
+		}
 	}
 	
 	else {
@@ -141,6 +188,7 @@ else {
 	<fieldset id="interior">
 		<legend>Statistics directory: ' . $path_to_statistics_directory . '</legend>
 	';
+		/*
 		$file_list = glob($path_to_statistics_directory . '*.conf');
 		foreach ($file_list as $file) {
 			if ($file != $common_parameters_file) {
@@ -149,7 +197,10 @@ else {
 				<input type="checkbox" id="checkbox_refresh" name="existing_list[]" value="' . $file . '"> ' . $file_name . '<br>';
 			}
 		}
+		*/
 	echo '
+	<div id="fileTree" class="tree"></div>
+	<input type="" class="input_text" name="statistics_to_refresh"/>
 	</fieldset>
 	<table align="right" style="overflow: auto; width: 100%" id="submit_button_table">
 		<tr>
